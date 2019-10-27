46d84c05d699834dfdf0595823dc58f91e175230
AladinBS <aladin.bensassi@gmail.com>
2019-10-27 18:14:38 +0000

    push

--- holberton.h
+++ holberton.h
@@ -1,21 +1,31 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef HOLB_H
+#define HOLB_H
+#define BUFSIZE 1025
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-typedef struct op
+/**
+* struct validTypes - structure to lookup functions for valid types
+* @valid: flags are preceded by a '%' character.
+* @f: pointer to function
+*/
+typedef struct validTypes
 {
-char op;
-void (*f)();
-} op_t;
-
+char *valid;
+char *(*f)();
+} v_types;
 int _printf(const char *format, ...);
-void print_char(va_list ap);
-void print_string(va_list ap);
-int _putchar(char c);
+char *(*get_valid_type(char s))(va_list);
+char *found_char(va_list c);
+char *found_string(va_list *s);
+char *found_percent();
+char *found_int(va_list n);
+char *found_unsigned(va_list usign);
+char *found_nothing(char);
+char *found_reverse(va_list s);
+char *found_rot13(va_list s);
+char *found_octal(va_list n);
+char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
 int _strlen(char *s);
-void print_int(int n);
-void print_mod(char c);
+void _puts(char *buffer, int size);
+int alloc_buffer(char *hold, int hlen, char *buffer, int blen, double *total);
+char *ctos(char c);
 #endif
