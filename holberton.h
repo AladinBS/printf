1b7e7ea6f00e5b42d5d37fa1f1955aa6b9775bdc
AladinBS <aladin.bensassi@gmail.com>
2019-10-28 13:22:59 +0000

    fix

--- holberton.h
+++ holberton.h
@@ -1,31 +1,20 @@
-#ifndef HOLB_H
-#define HOLB_H
-#define BUFSIZE 1025
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
 /**
-* struct validTypes - structure to lookup functions for valid types
-* @valid: flags are preceded by a '%' character.
-* @f: pointer to function
+* struct mystr - pointer to function with corresponding letter
+* @letter: specifier
+* @func: print argument
 */
-typedef struct validTypes
+typedef struct mystr
 {
-char *valid;
-char *(*f)();
-} v_types;
+char letter;
+int (*func)(va_list);
+} mystr;
+int _putchar(char c);
 int _printf(const char *format, ...);
-char *(*get_valid_type(char s))(va_list);
-char *found_char(va_list c);
-char *found_string(va_list *s);
-char *found_percent();
-char *found_int(va_list n);
-char *found_unsigned(va_list usign);
-char *found_nothing(char);
-char *found_reverse(va_list s);
-char *found_rot13(va_list s);
-char *found_octal(va_list n);
-char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
-int _strlen(char *s);
-void _puts(char *buffer, int size);
-int alloc_buffer(char *hold, int hlen, char *buffer, int blen, double *total);
-char *ctos(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int(*getspecifier(char))(va_list);
+int _print_i(va_list vi);
 #endif
