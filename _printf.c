856f96d8b2e01cabec865fcf024f34f159ef073d
AladinBS <aladin.bensassi@gmail.com>
2019-10-28 13:26:40 +0000

    fix

--- _printf.c
+++ _printf.c
@@ -1,70 +1,10 @@
-<<<<<<< HEAD
-#include<stdio.h>
-#include<stdarg.h>
-void Myprintf(char* format,...)
-{
-char *traverse;
-unsigned int i;
-char *s;
-va_list arg;
-va_start(arg, format);
-for(traverse = format; *traverse != '\0'; traverse++)
-{
-while( *traverse != '%' )
-{ 
-putchar(*traverse);
-traverse++;
-}
-traverse++;
-switch(*traverse) 
-{
-case 'c' : i = va_arg(arg,int);     //Fetch char argument
-putchar(i);
-break; 
-case 'd' : i = va_arg(arg,int);         //Fetch Decimal/Integer argument
-if(i<0) 
-{ 
-i = -i;
-putchar('-'); 
-} 
-puts(convert(i,10));
-break; 
-case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
-puts(convert(i,8));
-break;
-case 's': s = va_arg(arg,char *);       //Fetch string
-puts(s); 
-break; 
-case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
-puts(convert(i,16));
-break; 
-}   
-} 
-va_end(arg); 
-} 
-char *convert(unsigned int num, int base) 
-{ 
-static char Representation[]= "0123456789ABCDEF";
-static char buffer[50]; 
-char *ptr; 
-ptr = &buffer[49]; 
-*ptr = '\0'; 
-do 
-{ 
-*--ptr = Representation[num%base]; 
-num /= base; 
-}
-while(num != 0); 
-return(ptr); 
-=======
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
 /**
-* _printf - A print function
+* _printf - A print function 
 * @format: pointer to a string
 * Return: number of chars
-* Aladin Bensassi / Yassin Bahri
 **/
 int _printf(const char *format, ...)
 {
@@ -106,5 +46,4 @@ a++;
 }
 va_end(list);
 return (x);
->>>>>>> d4613ebff5f99cc8b93bd9d585501cbe23a4ded7
 }
