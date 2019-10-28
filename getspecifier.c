0780153d21b7be71d95b9936ba0c5589fd15a77c
student1043 <1043@holbertonschool.com>
2019-10-28 15:35:17 +0100

    Fix

--- getspecifier.c
+++ getspecifier.c
@@ -6,45 +6,23 @@
 * getspecifier - finds the function for the specifier
 * @x: the specifier in the string
 * Return: pointer to function
-* Aladin Bensassi / Yassin Bahri
 */
 int(*getspecifier(char x))(va_list)
 {
-int a;
+int i;
 mystr p[] = {
 {'c', print_c},
-{'s', print_s},  {'i', _print_i},
-{'d', _print_i},  {'\0', '\0'}
-{"b", print_bnr}, {"u", print_unt},
-{"o", print_oct}, {"x", print_hex},
-{"X", print_upx}, {"S", print_usr},
-{"p", print_add}, {"li", prinlint},
-{"ld", prinlint}, {"lu", prinlunt},
-{"lo", prinloct}, {"lx", prinlhex},
-{"lX", prinlupx}, {"hi", prinhint},
-{"hd", prinhint}, {"hu", prinhunt},
-{"ho", prinhoct}, {"hx", prinhhex},
-{"hX", prinhupx}, {"#o", prinnoct},
-{"#x", prinnhex}, {"#X", prinnupx},
-{"#i", print_int}, {"#d", print_int},
-{"#u", print_unt}, {"+i", prinpint},
-{"+d", prinpint}, {"+u", print_unt},
-{"+o", print_oct}, {"+x", print_hex},
-{"+X", print_upx}, {" i", prinsint},
-{" d", prinsint}, {" u", print_unt},
-{" o", print_oct}, {" x", print_hex},
-{" X", print_upx}, {"R", print_rot},
-{"r", print_rev}, {"%", print_prg},
-{"l", print_prg}, {"h", print_prg},
-{" +i", prinpint}, {" +d", prinpint},
-{"+ i", prinpint}, {"+ d", prinpint},
-{" %", print_prg}, {NULL, NULL},
+{'s', print_s},
+{'i', _print_i},
+{'d', _print_i},
+{'r', print_rs},
+{'\0', '\0'}
 };
-for (a = 0; p[a].letter; a++)
+for (i = 0; p[i].letter; i++)
 {
-if (p[a].letter == x)
+if (p[i].letter == x)
 {
-return (p[a].func);
+return (p[i].func);
 }
 }
 return (0);
