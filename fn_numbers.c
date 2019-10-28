b8d5da52d6490b3f77bda118dfc480af6554dfe8
student1043 <1043@holbertonschool.com>
2019-10-28 14:32:04 +0100

    Numbers

---
+++ fn_numbers.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*_print_i - prints an integer
+*@vi: the list of arguments
+* Return: number of printed characters
+* Aladin Ben Sassi / Yassin Bahri
+*/
+int _print_i(va_list vi)
+{
+int x, num, div, o, n, count = 0;
+n = va_arg(vi, int);
+o = n % 10;
+n = n / 10;
+if (o < 0)
+{
+n = -n;
+o = -o;
+_putchar('-');
+count++;
+}
+num = n;
+div = 1;
+if (num > 0)
+{
+while ((num / 10) != 0)
+{
+num = num / 10;
+div = div * 10;
+}
+while (div >= 1)
+{
+x = n / div;
+_putchar(x + '0');
+count++;
+n = n % div;
+div = div / 10;
+}
+}
+_putchar(o + '0');
+count++;
+return (count);
+}
