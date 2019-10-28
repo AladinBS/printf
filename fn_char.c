2bce02c796fe791742ed62f252826be9fc937856
student1043 <1043@holbertonschool.com>
2019-10-28 15:40:59 +0100

    Fix

--- fn_char.c
+++ fn_char.c
@@ -16,20 +16,46 @@ return (1);
 * print_s - prints a string and returns the length of string
 * @s: the list of arguments
 * Return: number of printed characters
-* Aladin Ben Sassi / Yassin Bahri
 */
 int print_s(va_list s)
 {
 char *string;
-int x = 0;
+int i = 0;
 string = va_arg(s, char *);
 if (string == '\0')
 {
 string = "(null)";
 }
-for (x = 0; string[x]; x++)
+for (i = 0; string[i]; i++)
 {
-_putchar(string[x]);
+_putchar(string[i]);
 }
-return (x);
+return (i);
+}
+/**
+* print_rs - prints the reversed string and returns the length of string
+* @rs: the list of arguments
+* Return: number of printed characters
+*/
+int print_rs(va_list rs)
+{
+char *string;
+int i = 0, count = 0;
+string = va_arg(rs, char *);
+if (string == '\0')
+{
+string = ")llun(";
+}
+for (i = 0; string[i]; i++)
+{
+;
+}
+i--;
+
+for (; i >= 0; i--)
+{
+_putchar(string[i]);
+count++;
+}
+return (count);
 }
