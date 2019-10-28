935f8b4fd948afe31133977496145482813a7d95
student1043 <1043@holbertonschool.com>
2019-10-28 14:49:36 +0100

    Fixing putchar'

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
 
 /**
-<<<<<<< HEAD
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
+ * Aladin Ben Sassi / Yassin Bahri
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,15 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-=======
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
->>>>>>> d4613ebff5f99cc8b93bd9d585501cbe23a4ded7
