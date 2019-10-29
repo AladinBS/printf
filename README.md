d3804cda8e11b8f94b39bd3b88517306267611fa
AladinBS <aladin.bensassi@gmail.com>
2019-10-29 18:07:23 +0000

    General fixes

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# PRINTF project
-
-A holberton school project. Create by Aladin Bensassi / Yasin Bahri
-
-## Compilation
-
-To compile use:
-
-```bash
-$ gcc -Wall -Werror -Wextra -pedantic *.c
-```
-
-
-## License
-[MIT](https://choosealicense.com/licenses/mit/)
+This is my readme file for now
--- holberton.h
+++ holberton.h
@@ -1,74 +1,79 @@
-#ifndef PRINT_F
-#define PRINT_F
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <stdarg.h>
 #include <unistd.h>
-
-#define LENGTH(a) (sizeof(a) / sizeof((a)[0]))
-
-/**
-* struct Options - format specifier options
-* @minus: -, left align
-* @plus: +, display + before non-negative numbers
-* @space:  , display space before non-negatives
-* @hash: o: display 0/0x/0X before nonzero numbers or force decimal point
-* @pad: character to fill empty space with
-* @length: length
-* @precision: precision, or max string length
-* @size: size modifier
-*/
-typedef struct Options
-{
-int minus;
-int plus;
-int space;
-int hash;
-char pad;
-int length;
-int precision;
-int size;
-} Options;
-
-typedef void (*Printer)(va_list, Options);
+#include <stdlib.h>
+#define HASH_FLAG 0x10
+#define JUST_FLAG 0x8
+#define PLUS_FLAG 0x4
+#define SPACE_FLAG 0x2
+#define ZERO_FLAG 0x1
+#define BUF_SIZE 1024
 
 /**
-* struct Spec - function pointer and specifier.
-* @name: name of specifier.
-* @f: function pointer for get spec.
-*
-* Description: Specifier types.
-*/
-typedef struct Spec
+ * struct spec - Struct to hold format info for string conversion functions
+ * @flags: small integer used as a bitfield to store flags from format string
+ * @length: `l' of `\0' depending on whether `long' is found in length field
+ * @width: minimum number of characters to output +/- padding
+ * @prec: precision to print for floats, or bytes to print from string
+ * @stride: total amount of format info "digested" by format parser
+ * @func: function to use for printing next argument from `va_list'
+ */
+struct spec
 {
-char name;
-Printer f;
-} Spec;
-
-int out(const char *data, int length);
-void outc(char c);
-void outcr(char c, int count);
-void pad_before(Options options, int length, char *prefix, int prefixlen);
-void pad_after(Options options, int length);
-
+	char flags;
+	char length;
+	int width;
+	int prec;
+	int stride;
+	int (*func)(struct spec *, va_list);
+};
+/**
+ * spec_t - typedef for struct `spec'
+ */
+typedef struct spec spec_t;
+/**
+ * struct conv_func - struct to hold format specifier symbol and
+ * associated string conversion function.
+ * @symbol: format string symbol (e.g. `s' or `d')
+ * @func: function to convert specified argument type
+ */
+struct conv_func
+{
+	char symbol;
+	int (*func)(spec_t *, va_list);
+};
+/**
+ * conv_func_t - typedef for struct `conv_func'
+ */
+typedef struct conv_func conv_func_t;
+void get_spec(const char *, spec_t *);
+int setflags(const char *, spec_t *);
+int setwidth(const char *, spec_t *);
+int setprec(const char *, spec_t *);
+int setlength(const char *, spec_t *);
+int settype(const char *, spec_t *);
+int conv_pc(spec_t *, va_list);
+int conv_i(spec_t *, va_list);
+int conv_u(spec_t *, va_list);
+int conv_b(spec_t *, va_list);
+int conv_f(spec_t *, va_list);
+int conv_e(spec_t *, va_list);
+int conv_E(spec_t *, va_list);
+int conv_x(spec_t *, va_list);
+int conv_X(spec_t *, va_list);
+int conv_o(spec_t *, va_list);
+int conv_s(spec_t *, va_list);
+int conv_S(spec_t *, va_list);
+int conv_c(spec_t *, va_list);
+int conv_p(spec_t *, va_list);
+int conv_a(spec_t *, va_list);
+int conv_A(spec_t *, va_list);
+int conv_r(spec_t *, va_list);
+int _isdigit(char);
+int write_hex(unsigned char);
+int isprintable(unsigned char);
 int _printf(const char *format, ...);
-
-void process_item(const char **format, va_list args);
-
-void output_invalid(Options options, char spec);
-
-Printer get_spec(char c);
-
-#define GET_SIZED(n, options, args, type) do {			\
-if (options.size == 2)													\
-n = (short type) va_arg(args, type);					\
-else if (options.size == 3)											\
-n = va_arg(args, long type);									\
-else																						\
-n = va_arg(args, type);												\
-} while (0)
-
-void print_r(va_list args, Options options)
-
-#endif
+unsigned int writetobuf(char *, unsigned int);
+int conv_base(char *, int, unsigned long int, ...);
+#endif /* _HOLBERTON_H_ */
