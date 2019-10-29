8e5b057c2a62b790b0f17f397bbe0ea560becce7
AladinBS <aladin.bensassi@gmail.com>
2019-10-29 16:45:11 +0000

    General fixes

--- fn_numbers.c
+++ fn_numbers.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 /**
 *_print_i - prints an integer
 *@vi: the list of arguments
@@ -41,74 +40,6 @@ div = div / 10;
 }
 _putchar(o + '0');
 count++;
-return (count);
-}
-
-/**
-* print_u - Convert a number into an unsigned int and print it
-* @un: The number to be converted
-*
-* Return: The number of digits printed
-*/
-int print_u(va_list un)
-{
-unsigned int i, num, div, o, n, count = 0;
-
-n = va_arg(u, unsigned int);
-o = n % 10;
-n = n / 10;
-num = n;
-div = 1;
-if (num > 0)
-{
-while ((num / 10) != 0)
-{
-num = num / 10;
-div = div * 10;
-}
-while (div >= 1)
-{
-i = n / div;
-_putchar(i + '0');
-count++;
-n = n % div;
-div = div / 10;
-}
-}
-_putchar(o + '0');
-count++;
-return (count);
-}
-
-/**
- * print_oct - Converts a decimal number passed to the argument to an octal
- * number
- * @oct: The number to be converted
- * Return: count of digit in octal number
- */
-int print_oct(va_list oct)
-{
-unsigned int octal[10];
-unsigned int count = 0, n = 0;
-int i = 0;
-
-n = va_arg(o, unsigned int);
-if (n == 0)
-{
-_putchar(0 + '0');
-count++;
-}
-while (n > 0)
-{
-octal[i] = n % 8;
-n = n / 8;
-i++;
-}
-for (--i; i >= 0; i--)
-{
-_putchar(octal[i] + '0');
-count++;
-}
 return (count);
 }
 
