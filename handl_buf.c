96dc749c824ab03813ed987d4162e4a9cad4feee
AladinBS <aladin.bensassi@gmail.com>
2019-10-29 18:44:23 +0000

    test

---
+++ handl_buf.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* handl_buf - concatenates the buffer characters
+* @buf: buffer pointer
+* @c: charcter to concatenate
+* @ibuf: index of buffer pointer
+* Return: index of buffer pointer.
+*/
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
+{
+if (ibuf == 1024)
+{
+print_buf(buf, ibuf);
+ibuf = 0;
+}
+buf[ibuf] = c;
+ibuf++;
+return (ibuf);
+}
